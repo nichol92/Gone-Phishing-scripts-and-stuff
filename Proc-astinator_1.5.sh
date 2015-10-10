86e8305cd7e5cbdad8e060910dc6f2a593423688
nichol92 <15nicholsa@gmail.com>
2015-10-10 17:19:23 +0100

    Finished the proc routines
    Added functionality for the top command allowing users to view realtime statistics about the process's.

--- Proc-astinator_1.5.sh
+++ Proc-astinator_1.5.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 function proc # this function takes not of the current directory and then switches to proc
 {
-directory="$(pwd)" # finds out where the script is being run from
+#directory="$(pwd)" # finds out where the script is being run from : code may be useful in the future
 cd ../../../.. #changes the directory to proc
 cd /proc/
-options
+options 
 }
 function options
 {
+echo "======================================"
+echo "-----------proc-astinator-------------"
+echo "======================================"
+echo '[*] Type "help" for a list of commands'
+# lets the user type a command and then selects and runs the corresponding function
 read com
 case $com in
 help)
@@ -41,27 +46,37 @@ options
 
 function primary_mem 
 {
-echo "*----Current-Proccesses-info---------*"
+echo "*-----Proccesses----------*"
 IFS=$'\n'	
 arr=($(ls))	# reads all the results from ls into an area and ignore's any spaces
 for i in "${arr[@]}" # cycles through all the elements in the array
 do
- if [[ $i =~ [0-9] ]] #checks if the current element is an integer
+#prints all of the process PID's name and there current state from the proc directory
+ if [[ $i =~ [0-9] ]] 
 	then
 		if [ -d "$i" ]; then
 		cd $i
 		name=$(head -n 1 status)
 		state=$(sed '2q;d' status)
-		echo 'PID:' $i $name $state #prints the string if its an integer, if its not the sting is ignored.
+		echo 'PID:' $i $name  $state #prints the string if its an integer, if its not the sting is ignored.
 		cd ..
 				fi
 	fi
 done
 echo ""
+echo "======================================"
+echo "-------Processes-usage----------------"
+echo "======================================"
+echo "press ctrl + c to return to the script"
+echo ""
+top #runs the top command
+
 
 echo "======================================"
 echo "------Thats-everything-here--:)-------"
 echo "======================================"
+echo ""
+
 options
 
 }
@@ -73,34 +88,40 @@ echo "======================================"
 echo "-------CPU-information----------------"
 echo "======================================"
 echo ""
-cat cpuinfo
+cat cpuinfo # retrieves all the information from the cpuinfo file in proc
 echo '------average load of the cpu---------'
 echo ""
-cat loadavg
+cat loadavg # retrieves all the information from the loadavg file in proc
+
 echo "======================================"
 echo "---------Connected-Devices------------"
 echo "======================================"
 echo ""
-cat devices
+cat devices# retrieves all the information from the devices file in proc
+
 echo "======================================"
 echo "---------i/o-ports--------------------"
 echo "======================================"
 echo ""
-cat ioports
+cat ioports# retrieves all the information from the ioports file in proc
 echo "======================================"
 echo "---------memory-information-----------"
 echo "======================================"
 echo ""
-cat meminfo
+cat meminfo # retrieves all the information from the meminfo file in proc
+
 echo "======================================"
 echo "--------i/o-memory-ranges-------------"
 echo "======================================"
 echo ""
-cat iomem
+cat iomem # retrieves all the information from the iomem file in proc
+
 echo "======================================"
 echo "------Thats-everything-here--:)-------"
 echo "======================================"
-options
+echo ""
+
+options #returns to start of program
 
 }
 function sys
@@ -111,31 +132,36 @@ echo "======================================"
 echo "------OS-version----------------------"
 echo "======================================"
 echo ""
-cat version
+cat version # retrieves all the information from the version file in proc
+
 echo "------system-uptime-------------------"
 echo ""
-cat uptime
+cat uptime # retrieves all the information from the uptime file in proc
+
 echo "======================================"
 echo "---------File-systems-----------------"
 echo "======================================"
 echo ""
-cat filesystems
+cat filesystems # retrieves all the information from the filesystems file in proc
+
 echo "======================================"
 echo "-----------Partitions-----------------"
 echo "======================================"
 echo ""
-cat partitions
+cat partitions # retrieves all the information from the partitions file in proc
+
 echo "======================================"
 echo "------Thats everything here--:)-------"
 echo "======================================"
-options
+echo ""
+
+options #returns to the start of the program
 
 }
-echo "======================================"
-echo "-----------proc-astinator-------------"
-echo "======================================"
-echo '[*] Type "help" for a list of commands'
-
-proc # runs this function
+proc # starts the proc function
+function dev #this is will contain nishan's code
+{
+	echo "Under construction"
+}
 
 
